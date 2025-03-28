5093fee450f6b1dd5a052cb3346317fa3772f430
Shataevents <shata.app1@gmail.com>
2025-03-28 19:25:55 +0530

    updation of firebase project

--- src/firebase.js
+++ src/firebase.js
@@ -31,17 +31,18 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDYN3SV_IApa52ftkGQE67D8FwbWNJIBFo",
-  authDomain: "shata-bd22f.firebaseapp.com",
-  projectId: "shata-bd22f",
-  storageBucket: "shata-bd22f.appspot.com",
-  messagingSenderId: "926646791500",
-  appId: "1:926646791500:web:a21bc2edb7923efff9f3e9",
-  measurementId: "G-QV6H6881CM"
+  apiKey: "AIzaSyBUZis7DcJ1L8J6UzMt0O89htcNPL9Lde4",
+  authDomain: "shataadmin.firebaseapp.com",
+  projectId: "shataadmin",
+  storageBucket: "shataadmin.firebasestorage.app",
+  messagingSenderId: "900557092345",
+  appId: "1:900557092345:web:0219e5d57afa71983df928",
+  measurementId: "G-0V0B7GE6EG"
 };
 
 // ✅ Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const auth = getAuth(app);
 
 export { auth };
